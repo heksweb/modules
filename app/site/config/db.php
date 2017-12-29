dbbd2c710d09e68adbc32c1d926ea426b109d88a
heksweb <heksweb@gmail.com>
2017-12-29 13:33:27 +0300

    Configured db.php file

--- app/site/config/db.php
+++ app/site/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysqldb;dbname=modules',
+    'username' => 'modules',
+    'password' => 'qwerty',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
